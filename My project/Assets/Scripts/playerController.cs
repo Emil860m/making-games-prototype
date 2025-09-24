e1a1a3db1d6cefcd5e3fc3e21575e9587740e49e
Emil <fenger.emil@gmail.com>
2025-09-24 20:44:36 +0200

    viewbobbin

--- My project/Assets/Scripts/playerController.cs
+++ My project/Assets/Scripts/playerController.cs
@@ -6,62 +6,74 @@ public class playerController : MonoBehaviour
 
     private bool rotateRight = false;
     private bool rotateLeft = false;
+    private bool moveForward = false;
 
     private float target_degrees = 0;
     private float r;
 
-    public float viewBobbinIntensity;
+    public ViewBobController viewBobSystem;
 
-    public int rotateSpeed = 5; 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    
+
+    public int speed = 5; 
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow) && !rotateRight)
+        if (Input.GetKeyDown(KeyCode.RightArrow) && !rotateRight && !rotateLeft && !moveForward)
         {
             rotateRight = true;
             target_degrees = target_degrees + 90;
+            viewBobSystem.SetViewBob(true);
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && !rotateLeft)
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && !rotateRight && !rotateLeft && !moveForward)
         {
             rotateLeft = true;
             target_degrees = target_degrees - 90;
+            viewBobSystem.SetViewBob(true);
         }
         // If we do extending rooms: 
-        /*
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow) && !rotateRight && !rotateLeft && !moveForward)
         {
-            transform.position = transform.position + (transform.forward * 16);
+            moveForward = true;
+            viewBobSystem.SetViewBob(true);
         }
-        */
         if (rotateRight)
         {
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target_degrees, ref r, 1f / rotateSpeed);
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target_degrees, ref r, 1f / speed);
             
             transform.rotation = Quaternion.Euler(0, angle, 0);
             if (transform.rotation.eulerAngles.y > target_degrees - 0.1 && transform.rotation.eulerAngles.y < target_degrees + 0.1)
             {
                 transform.rotation = Quaternion.Euler(0, target_degrees, 0);
                 rotateRight = false;
+                viewBobSystem.SetViewBob(false);
             }
         }
         if (rotateLeft)
         {
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target_degrees, ref r, 1f / rotateSpeed);
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target_degrees, ref r, 1f / speed);
             
             transform.rotation = Quaternion.Euler(0, angle, 0);
             if (transform.rotation.eulerAngles.y > target_degrees - 0.1 && transform.rotation.eulerAngles.y < target_degrees + 0.1)
             {
                 transform.rotation = Quaternion.Euler(0, target_degrees, 0);
                 rotateLeft = false;
+                viewBobSystem.SetViewBob(false);
             }
         }
         if (target_degrees >= 360) target_degrees -= 360;
         else if (target_degrees < 0) target_degrees += 360;
+        if (moveForward)
+        {
+            transform.position = transform.position + (transform.forward * Time.deltaTime * speed);
+            if (transform.position.z > 0 && target_degrees == 0)
+            {
+                transform.position = Vector3.zero;
+                moveForward = false;
+                viewBobSystem.SetViewBob(false);
+            }
+        }
     }
 }
