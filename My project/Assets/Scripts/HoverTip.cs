f77e43afa68ff086e65c07e0f7be86e485f77e6d
Daniel-EWE <dvh@ewe.dk>
2025-09-25 20:09:18 +0200

    Added hover functionalty so when you hover an gameObject you can see text. The styling is god awful, but the functionality works

---
+++ My project/Assets/Scripts/HoverTip.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverTip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public string tipToShow;
+    private float timeToWait = 0.5f;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        StopAllCoroutines();
+        StartCoroutine(StartTimer());
+        StartTimer();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        StopAllCoroutines();
+        HoverTipManager.OnMouseExit();
+    }
+    private void ShowTip()
+    {
+        HoverTipManager.OnMouseHover(tipToShow, Input.mousePosition);
+    }
+
+    private IEnumerator StartTimer()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        ShowTip();
+    }
+}
